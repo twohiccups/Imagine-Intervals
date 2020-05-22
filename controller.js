445a86cc89c9e81d74c1a8602fe6eb4c9315a212
twohiccups <koshkinmark@gmail.com>
2020-05-21 22:45:17 -0400

    feat: update

--- controller.js
+++ controller.js
@@ -1,15 +1,25 @@
+var useIntervalNames = true;
+
 function createIntervalButtons() {
-    
-    $('#active-intervals').append('<button>h</button>')   
+
+    //    $('#active-intervals').append('<button>h</button>')   
 }
 
 
 $(window).on('load', () => {
-    
-    createIntervalButtons()
 
-    
-    $('#magnitude').text(getDirectionText());
+    setIntervalButtonNames();
+
+    $('#app-info').on('click', () => {
+        $('#app-info').hide();
+        $('#outer').removeClass('outer-init')
+        $('#outer').addClass('outer')
+        $('#middle').addClass('middle')
+        $('#inner').addClass('inner')
+    })
+
+
+    $('#magnitude').text(getDirectionButtonText());
 
     $('#repeat-note').on('click', () => {
         if (currentNote) {
@@ -18,30 +28,71 @@ $(window).on('load', () => {
     })
     $('#play-answer').on('click', () => {
         if (currentNote) {
-            playAnswer(currentNote)   
+            playAnswer(currentNote)
         };
     })
     $('#play-together').on('click', () => {
         if (currentNote) {
-            playTogether(currentNote)   
+            playTogether(currentNote);
         };
     })
-        
+//
+//    $('.interval-button').on('click', (e) => {
+//        alert($('.interval-button').index($(this)))
+//    })
     
+    $('.interval-button').on('click', function() {
+        $(this).toggleClass('interval-off');
+        
+        const enabled =  ratiosMap[$(this).index() + 1].enabled
+        ratiosMap[$(this).index() + 1].enabled = !enabled;  
+    })
+    $("#interval-names").on('click', () => {
+        useIntervalNames = !useIntervalNames;
+        setIntervalButtonNames();
+        $('#interval-name').text(getIntervalText());
+        if (useIntervalNames) {
+            $("interval-names").text("Switch To Names")
+        } else {
+            $("interval-names").text("Switch To Numbers")
+        }
+    });
+
     $('#magnitude').on('click', () => {
         changeMagnitude()
-        $('#magnitude').text(getDirectionText())
+        $('#magnitude').text(getDirectionButtonText())
     })
-    
+
 })
 
+function setIntervalButtonNames() {
+    for(i = 0; i < 11; i++) {
+        if (useIntervalNames) {
+            $('.interval-button').eq(i).text(ratiosMap[i + 1].name)
+        } else {
+            $('.interval-button').eq(i).text(ratiosMap[i + 1].number)
+        }
+    }
+}
 
 
-function getDirectionText() {
+function getIntervalText() {
+    var intervalName = useIntervalNames ? currentInterval.name : currentInterval.number;
+    var intervalText = Direction[currentDirection].description + ' ' + intervalName;
+    return intervalText;
+}
+
+function handleIntervalNameUI() {
+    $('#interval-name').removeClass('press-space');
+}
+
+
+function getDirectionButtonText() {
     return 'Direction: ' + Direction[magnitudeChoice].description;
 }
 
 
 function handleSpaceUI() {
-    $('#interval-name').text(Direction[currentDirection].description + ' ' + currentInterval.name);
+    const intervalText = useIntervalNames ? currentInterval.name : currentInterval.number;
+    $('#interval-name').text(getIntervalText());
 };
--- main.js
+++ main.js
@@ -3,18 +3,18 @@ const synth2 = new Tone.Synth().toMaster();
 
 
 const ratiosMap = [
-    {name: "P1", enabled: false, ratio: 1},
-    {name: "m2", enabled: true, ratio: 16 / 15},
-    {name: "M2", enabled: true, ratio: 9 / 8},
-    {name: "m3", enabled: true, ratio: 6 / 5},
-    {name: "M3", enabled: true, ratio: 5 / 4},
-    {name: "P4", enabled: true, ratio: 4 / 3},
-    {name: "A4", enabled: true, ratio: 45 / 32},
-    {name: "P5", enabled: true, ratio: 3 / 2},
-    {name: "m6", enabled: true, ratio: 8 / 5},
-    {name: "M6", enabled: true, ratio:  5 / 3},
-    {name: "m7", enabled: true, ratio:  16 / 9},
-    {name: "M7", enabled: true, ratio:  15 / 8}
+    {name: "P1", number: "0", enabled: false, ratio: 1},
+    {name: "m2", number: "1", enabled: true, ratio: 16 / 15},
+    {name: "M2", number: "2", enabled: true, ratio: 9 / 8},
+    {name: "m3", number: "3", enabled: true, ratio: 6 / 5},
+    {name: "M3", number: "4", enabled: true, ratio: 5 / 4},
+    {name: "P4", number: "5", enabled: true, ratio: 4 / 3},
+    {name: "A4", number: "6", enabled: true, ratio: 45 / 32},
+    {name: "P5", number: "7", enabled: true, ratio: 3 / 2},
+    {name: "m6", number: "8", enabled: true, ratio: 8 / 5},
+    {name: "M6", number: "9", enabled: true, ratio:  5 / 3},
+    {name: "m7", number: "10",  enabled: true, ratio:  16 / 9},
+    {name: "M7", number: "11",  enabled: true, ratio:  15 / 8}
 ]
 
 
@@ -136,6 +136,7 @@ $(document).on('keypress', (e) => {
     if (e.code == 'Space') {
         e.preventDefault()
         handleGenerateAndPlay();
+        handleIntervalNameUI();
     }
     if (e.code == 'KeyZ') {
         playNote(currentNote);
